0323aa1d07ac63077c6db7b9d5e2d04b4735c677
kohemf1 <kohemf1@gmail.com>
2021-05-24 16:03:18 +0900

    Update Calculator.java

--- java_project/MyProject/src/exam/Calculator.java
+++ java_project/MyProject/src/exam/Calculator.java
@@ -1,5 +1,22 @@
 package exam;
 
-public class Calculator {
+	class Calculator{
+		
+		int a;
+		int b;
+		
+		int add (int a , int b) {
+			return a + b;
+		}
+		int sub (int a , int b) {
+			return a - b;
+		}
+		int mul (int a , int b) {
+			return a*b;
+		}
+		double div (int a , int b) {
+			return (int) a / b;
+		}
 
-}
+	}
+
