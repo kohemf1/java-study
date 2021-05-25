18a576a400b02202b4a02f21faa843ddd2d32b36
kohemf1 <kohemf1@gmail.com>
2021-05-25 12:02:49 +0900

    ch03
    ch03 답안

---
+++ java_project/Java1/src/ch03/Calculator.java
@@ -0,0 +1,32 @@
+package ch03;
+
+import java.util.Scanner;
+
+public class Calculator {
+
+	 int add(int a, int b) { return a+b; }
+	 int sub(int a, int b) { return a-b; }
+	 int mul(int a, int b) { return a*b; }
+	 float div(int a, int b) { return (float)a/b; }
+	 float cif (int r) { return (float)( 2*3.14*r);}
+	 float ext (int r) { return (float)( 3.14*r*r);}
+
+	public static void main(String[] args) {
+		
+		Calculator cal = new Calculator();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("첫번째 숫자를 입력해주세요.");
+		int a = sc.nextInt();
+		System.out.println("두번째 숫자를 입력해주세요.");
+		int b = sc.nextInt();
+		System.out.println("반지름을 입력해주세요.");
+		int r = sc.nextInt();
+		System.out.println("더하기 : " + cal.add(a,b));
+		System.out.println("빼기 : " + cal.sub(a,b));
+		System.out.println("곱하기 : " + cal.mul(a,b));
+		System.out.println("나누기 : " + cal.div(a,b));
+		System.out.println("원의 둘레 : " + cal.cif(r));
+		System.out.println("원의 넓이 : " + cal.ext(r));
+			
+	}
+}
