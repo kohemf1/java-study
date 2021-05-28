58cb4227d42761a894d9c94e17412959ac53105c
kohemf1 <kohemf1@gmail.com>
2021-05-28 17:37:21 +0900

    상속활용
    상속을 이용하여 개인 정보 관리 프로그램 만들기

---
+++ java_project/Java1/src/ex/infomanager/HighFriend.java
@@ -0,0 +1,26 @@
+package ex.infomanager;
+
+public class HighFriend extends Friend {
+
+	//고교친구는 직장에서 어떤일을 하는지
+	String work;
+	
+	public HighFriend(String name, String phoneNumber, String address, String work) {
+		super(name, phoneNumber, address);
+		this.work = work;
+	}
+
+	@Override 
+	public void showData() {
+		super.showData();
+		System.out.println(" 직   업  :" + work);
+	}
+
+	@Override
+	public void showBasicInfo() {
+		System.out.println("이름 : " + name);
+		System.out.println("전화 : " + phoneNumber);
+	}
+	
+	
+}
