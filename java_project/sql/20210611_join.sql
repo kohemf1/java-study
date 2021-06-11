df01f66d24021fadd303e4ca285591ed029a1991
kohemf1 <kohemf1@gmail.com>
2021-06-11 18:06:07 +0900

    join
    join practice

---
+++ java_project/sql/20210611_join.sql
@@ -0,0 +1,20 @@
+-- 2021.06.11
+
+-- join
+
+-- cross join
+-- 스키마의 합 : 집합의 곱 연산과 같다
+
+select *
+from emp, dept
+where emp.deptno = dept.deptno 
+;
+
+-- 이름이 SCOTT인 사원( = where)의 이름과 부서명 출력
+-- 출력해야하는 컬럼을 가지는 테이블을 확인해보자.
+-- 이름 : emp, 부서명 : dept
+SELECT e.ename,d.dname
+FROM EMP e,DEPT d
+WHERE e.DEPTNO = d.DEPTNO --AND e.ENAME = 'SCOTT'
+ORDER BY d.dname asc, e.ename asc
+;
