31231e7cde88d2681b41016abbc3811fbf45ff8e
kohemf1 <kohemf1@gmail.com>
2021-06-11 18:09:08 +0900

    join

--- java_project/sql/20210611_join.sql
+++ java_project/sql/20210611_join.sql
@@ -13,7 +13,7 @@ where emp.deptno = dept.deptno
 -- 이름이 SCOTT인 사원( = where)의 이름과 부서명 출력
 -- 출력해야하는 컬럼을 가지는 테이블을 확인해보자.
 -- 이름 : emp, 부서명 : dept
-SELECT e.ename,d.dname
+SELECT e.ename,d.dname, e.deptno
 FROM EMP e,DEPT d
 WHERE e.DEPTNO = d.DEPTNO --AND e.ENAME = 'SCOTT'
 ORDER BY d.dname asc, e.ename asc
