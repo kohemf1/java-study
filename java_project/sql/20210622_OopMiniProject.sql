9b3af7f561b00a983f5b8f23a52bf81e475a2bcf
kohemf1 <84695772+kohemf1@users.noreply.github.com>
2021-06-24 02:00:02 +0900

    idx -> code 변경

--- java_project/sql/20210622_OopMiniProject.sql
+++ java_project/sql/20210622_OopMiniProject.sql
@@ -1,7 +1,7 @@
 --OopMiniProject
 
 create table member(
-idx number(4) constraint member_idx_pk primary key,
+membercode number(4) constraint member_mmcode_pk primary key,
 id varchar2(20) not null,
 pw varchar2(20) not null,
 name varchar2(20) not null,
@@ -13,24 +13,24 @@ address varchar2(40) not null
 desc member;
 select * from member;
 
-CREATE SEQUENCE member_idx_SEQ
+CREATE SEQUENCE member_membercode_SEQ
 INCREMENT BY 1
 START WITH 1;
 
 insert into member values
-(1, 'member1', '123456', '손흥민', '123456-1234567', 'member1@naver.com', 'SEOUL');
+(MEMBER_MEMBERCODE_SEQ.nextval, 'member1', '123456', '손흥민', '123456-1234567', 'member1@naver.com', 'SEOUL');
 insert into member values
-(2, 'member2', '123456', '케인', '234567-1345678', 'member2@google.com', 'PUSAN');
+(MEMBER_MEMBERCODE_SEQ.nextval, 'member2', '123456', '케인', '234567-1345678', 'member2@google.com', 'PUSAN');
 insert into member values
-(3, 'member3', '123456', '모우라', '345678-1456789', 'member3@daum.net', 'DAEJEON');
+(MEMBER_MEMBERCODE_SEQ.nextval, 'member3', '123456', '모우라', '345678-1456789', 'member3@daum.net', 'DAEJEON');
 insert into member values
-(4, 'member4', '123456', '알리', '456789-1567890', 'member4@naver.com', 'JEJU');
+(MEMBER_MEMBERCODE_SEQ.nextval, 'member4', '123456', '알리', '456789-1567890', 'member4@naver.com', 'JEJU');
 insert into member values
-(5, 'member5', '123456', '베일', '567890-1678901', 'member5@google.com', 'SEOUL');
+(MEMBER_MEMBERCODE_SEQ.nextval, 'member5', '123456', '베일', '567890-1678901', 'member5@google.com', 'SEOUL');
 
 
 create table car(
-idx number(4) constraint car_idx_pk primary key,
+carcode number(4) constraint car_carcode_pk primary key,
 carnumber varchar2(20) not null,
 carname varchar2(20) not null,
 carsize varchar2(10) not null,
@@ -46,21 +46,23 @@ rent char(1) constraint car_rent_ck check(rent = '0' or rent = '1')
 desc car;
 select * from car;
 
-insert into car values(1, '1111', 'RAY', 'SMALL', 5, 2017, '가솔린','1');
-insert into car values(2, '2222', 'NIRO EV', 'SMALL', 5, 2022, '전기','1');
-insert into car values(3, '3333', 'SONATA', 'MIDDLE', 5, 2020, '가솔린','1');
-insert into car values(4, '4444', 'MODEL3', 'MIDDLE', 5, 2020, '전기','1');
-insert into car values(5, '5555', 'BMW X7', 'BIG', 7, 2020, '가솔린','1');
-insert into car values(6, '6666', 'CARNIVAL', 'BIG', 9, 2021, '가솔린','1');
+insert into car values(car_carcode_SEQ.nextval, '1111', 'RAY', 'SMALL', 5, 2017, '가솔린','1');
+insert into car values(car_carcode_SEQ.nextval, '2222', 'NIRO EV', 'SMALL', 5, 2022, '전기','1');
+insert into car values(car_carcode_SEQ.nextval, '3333', 'SONATA', 'MIDDLE', 5, 2020, '가솔린','1');
+insert into car values(car_carcode_SEQ.nextval, '4444', 'MODEL3', 'MIDDLE', 5, 2020, '전기','1');
+insert into car values(car_carcode_SEQ.nextval, '5555', 'BMW X7', 'BIG', 7, 2020, '가솔린','1');
+insert into car values(car_carcode_SEQ.nextval, '6666', 'CARNIVAL', 'BIG', 9, 2021, '가솔린','1');
 
-CREATE SEQUENCE car_idx_SEQ
+CREATE SEQUENCE car_carcode_SEQ
 INCREMENT BY 1
 START WITH 1;
 
 select * from car;
 select * from manager;
+
+
 create table manager(
-idx number(4) constraint manager_idx_pk primary key,
+managercode number(4) constraint mcode_mcode_pk primary key,
 mid varchar2(20) not null,
 mpw varchar2(20) not null
 )
@@ -68,29 +70,30 @@ mpw varchar2(20) not null
 desc manager;
 select * from manager;
 
-insert into manager values(1, 'manager1', '123123');
-insert into manager values(2, 'manager2', '321321');
+insert into manager values(manager_managercode_SEQ.nextval, 'manager1', '123123');
+insert into manager values(manager_managercode_SEQ.nextval, 'manager2', '321321');
 
 
-CREATE SEQUENCE manager_idx_SEQ
+
+CREATE SEQUENCE manager_managercode_SEQ
 INCREMENT BY 1
 START WITH 1;
 
 -----------------------------------------------------
 create table rent(
-idx number(4) constraint rent_idx_pk primary key,
+rentcode number(4) constraint rent_rcode_pk primary key,
 pay INTEGER not null,
 rentperiod number(1) not null,
-carcode number constraint rent_carcode_fk REFERENCES car(idx),
-membercode number constraint rent_memcode_fk REFERENCES member(idx),
-managercode number constraint rent_manacode_fr REFERENCES manager(idx)
+carcode number constraint rent_carcode_fk REFERENCES car(carcode),
+membercode number constraint rent_memcode_fk REFERENCES member(membercode),
+managercode number constraint rent_manacode_fk REFERENCES manager(managercode)
 )
 ;
 
 desc rent;
 select * from rent;
 
-CREATE SEQUENCE rent_idx_SEQ
+CREATE SEQUENCE rent_rentcode_SEQ
 INCREMENT BY 1
 START WITH 1;
 
@@ -100,8 +103,8 @@ commit;
 --drop table car;
 --drop table member;
 --drop table manager;
---drop sequence manager_idx_seq;
---drop sequence member_idx_seq;
---drop sequence Car_idx_seq;
---drop sequence rent_idx_seq;
+--drop sequence manager_managercode_seq;
+--drop sequence member_membercode_seq;
+--drop sequence Car_carcode_seq;
+--drop sequence rent_rentcode_seq;
 -----------------------------------------------------
