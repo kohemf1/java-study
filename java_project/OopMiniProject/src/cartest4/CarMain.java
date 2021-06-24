d2cbc7a864208988cb841dfff4b6265e109d303b
kohemf1 <84695772+kohemf1@users.noreply.github.com>
2021-06-24 11:16:22 +0900

    rentList , availableList
    rentlist , availablelist 추가

---
+++ java_project/OopMiniProject/src/cartest4/CarMain.java
@@ -0,0 +1,32 @@
+package cartest4;
+
+
+public class CarMain {
+
+	public static void main(String[] args) {
+
+
+		CarManager Cmanager = new CarManager(CarDao.getInstance());
+		
+		//Scanner sc = new Scanner(System.in);
+		
+		// 드라이버 로드
+		
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+//		Cmanager.CarList(); // 카 리스트 출력			
+//		Cmanager.rentCar(); // 자동차 대여
+//		Cmanager.returnCar(); // 자동차 반납
+//		Cmanager.inputCarData(); // 자동차 추가
+//		Cmanager.editCar(); // 자동차 정보 수정
+//		Cmanager.delCar(); // 자동차 삭제
+//		Cmanager.rentList(); // 대여중인 차량
+//		Cmanager.availableList(); // 대여 가능 차량
+	}
+
+}
