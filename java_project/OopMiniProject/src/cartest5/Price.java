e6a42aa0ebbc7c6d0680681a8baaf945817d23a5
kohemf1 <84695772+kohemf1@users.noreply.github.com>
2021-06-25 12:47:28 +0900

    20210625

---
+++ java_project/OopMiniProject/src/cartest5/Price.java
@@ -0,0 +1,54 @@
+package cartest5;
+
+public class Price {
+
+	
+		private int pnum;
+		private int price;
+		private int peiod;
+		private int sum;
+	
+		public Price(int pnum, int price, int peiod, int sum) {
+			this.pnum = pnum;
+			this.price = price;
+			this.peiod = peiod;
+			this.sum = sum;
+		}
+
+		public int getPnum() {
+			return pnum;
+		}
+
+		public void setPnum(int pnum) {
+			this.pnum = pnum;
+		}
+
+		public int getPrice() {
+			return price;
+		}
+
+		public void setPrice(int price) {
+			this.price = price;
+		}
+
+		public int getPeiod() {
+			return peiod;
+		}
+
+		public void setPeiod(int peiod) {
+			this.peiod = peiod;
+		}
+
+		public int getSum() {
+			return sum;
+		}
+
+		public void setSum(int sum) {
+			this.sum = sum;
+		}
+		
+		
+		
+	
+
+}
---
+++ java_project/OopMiniProject/src/cartest6/Car.java
@@ -0,0 +1,136 @@
+package cartest6;
+
+public class Car {
+	
+	int carcode;
+
+	private String carnumber;
+	private String carname;
+	private String carsize;
+	private int carseat;
+	private int caryear;
+	private String fuel;
+	private String rent;
+	
+	
+	public Car(int carcode, String carnumber, String carname,
+			String carsize, int carseat, int caryear, String fuel, String rent) {
+		super();
+		this.carcode = carcode;
+		this.carnumber = carnumber;
+		this.carname = carname;
+		this.carsize = carsize;
+		this.carseat = carseat;
+		this.caryear = caryear;
+		this.fuel = fuel;
+		this.rent = rent;
+	}
+
+
+
+	public int getCarcode() {
+		return carcode;
+	}
+
+
+
+	public void setCarcode(int carcode) {
+		this.carcode = carcode;
+	}
+
+
+
+	public String getCarnumber() {
+		return carnumber;
+	}
+
+
+
+	public void setCarnumber(String carnumber) {
+		this.carnumber = carnumber;
+	}
+
+
+
+	public String getCarname() {
+		return carname;
+	}
+
+
+
+	public void setCarname(String carname) {
+		this.carname = carname;
+	}
+
+
+
+	public String getCarsize() {
+		return carsize;
+	}
+
+
+
+	public void setCarsize(String carsize) {
+		this.carsize = carsize;
+	}
+
+
+
+	public int getCarseat() {
+		return carseat;
+	}
+
+
+
+	public void setCarseat(int carseat) {
+		this.carseat = carseat;
+	}
+
+
+
+	public int getCaryear() {
+		return caryear;
+	}
+
+
+
+	public void setCaryear(int caryear) {
+		this.caryear = caryear;
+	}
+
+
+
+	public String getFuel() {
+		return fuel;
+	}
+
+
+
+	public void setFuel(String fuel) {
+		this.fuel = fuel;
+	}
+
+
+
+	public String getRent() {
+		return rent;
+	}
+
+
+
+	public void setRent(String rent) {
+		this.rent = rent;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Car [carcode=" + carcode + ", carnumber=" + carnumber + 
+				", carname=" + carname + ", carsize=" + carsize
+				+ ", carseat=" + carseat + ", caryear=" + caryear +
+				", fuel=" + fuel + "]";
+	}
+	
+	
+}
