26a97ef3860277083544a514a32501b95d1f5f8c
kohemf1 <kohemf1@gmail.com>
2021-07-20 12:53:30 +0900

    mySql conn

--- java_project/JDBCTest/src/jdbc/JDBCTest.java
+++ java_project/JDBCTest/src/jdbc/JDBCTest.java
@@ -22,15 +22,25 @@ public class JDBCTest {
 
 		try {
 			//1. 드라이버 로드
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-
+			// 오라클
+//			Class.forName("oracle.jdbc.driver.OracleDriver");
+			// mysql
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
 			System.out.println("드라이버 로드 성공");
 
 			//2. 연결
-			String jdbcUrl1 = "jdbc:oracle:thin:@localhost:1521:xe";
-			String user = "hr";
-			String pw = "tiger";
+			// 오라클
+//			String jdbcUrl1 = "jdbc:oracle:thin:@localhost:1521:xe";
+			//mysql
+			String jdbcUrl1 = "jdbc:mysql://localhost:3306/project?serverTimezone=UTC";
+			
+//			String user = "hr";
+//			String pw = "tiger";
 
+			String user = "bit";
+			String pw = "bit";
+			
 			conn = DriverManager.getConnection(jdbcUrl1, user, pw);
 
 			System.out.println("데이터베이스 연결 성공!");
@@ -39,10 +49,11 @@ public class JDBCTest {
 			stmt = conn.createStatement();
 			
 			int dno = 10;
-			String otype = "deptno";
-			String sqlSelect = "Select * from dept where deptno = "+dno+" order by " + otype;
-
 			
+			String otype = "deptno";
+			
+//			String sqlSelect = "Select * from dept where deptno = "+dno+" order by " + otype;
+			String sqlSelect = "select * from dept";
 			
 			rs = stmt.executeQuery(sqlSelect);
 			
