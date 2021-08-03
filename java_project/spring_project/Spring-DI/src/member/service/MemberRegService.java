fc71474c582729405af101857e82f52535fc15e2
kohemf1 <84695772+kohemf1@users.noreply.github.com>
2021-08-03 12:58:04 +0900

    210803 spring Autowired && Resource

--- java_project/spring_project/Spring-DI/src/member/service/MemberRegService.java
+++ java_project/spring_project/Spring-DI/src/member/service/MemberRegService.java
@@ -9,10 +9,26 @@ import member.domain.RegRequest;
 public class MemberRegService {
 
 	//Dao dao = new MemberDao();  
-	Dao dao ;
+	private Dao dao ;
+	private int num;
 	
+	// 프로퍼티 방식의 주입 : setter 메소드를 이용, 기본생성자가 필요
 	public void setDao(Dao dao) {
 		this.dao = dao;
+	}
+	
+	/////////////////////////////////////////////
+	// 생성자의 정의
+	// 정수데이터를 받는 생성자
+	
+	// 기본 생성장
+	
+	public MemberRegService() {
+		System.out.println("MemberRegService");
+	}
+	public MemberRegService(int num) {
+		//this.dao = dao;
+		System.out.println("MemberRegService 인스턴스 생성");
 	}
 	
 	public MemberRegService(Dao dao) {
@@ -20,6 +36,12 @@ public class MemberRegService {
 		System.out.println("MemberRegService 인스턴스 생성");
 	}
 	
+	public MemberRegService(Dao dao, int num) {
+		this.dao = dao;
+		this.num = num;
+		System.out.println("MemberRegService 인스턴스 생성");
+	}
+	//////////////////////////////////////////////////
 	public void regMember(RegRequest request) throws Exception {
 		
 		// 중복 이메일 체크          
