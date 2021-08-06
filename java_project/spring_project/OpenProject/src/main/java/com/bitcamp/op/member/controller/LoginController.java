c48db12f35626a9bdfc60744cb97661092365c9a
kohemf1 <84695772+kohemf1@users.noreply.github.com>
2021-08-06 18:21:59 +0900

    210806 OpenProject Login

---
+++ java_project/spring_project/OpenProject/src/main/java/com/bitcamp/op/member/controller/LoginController.java
@@ -0,0 +1,23 @@
+package com.bitcamp.op.member.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.bitcamp.op.member.service.LoginService;
+
+@Controller
+@RequestMapping("/member/login")
+public class LoginController {
+	
+	private LoginService loginService;
+	
+	
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public String loginForm() {
+		return "member/loginForm";
+	}
+	
+	
+}
